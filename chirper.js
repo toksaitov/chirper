fb1e868bd01cd3414ef2f1c8598d343046ad0ad2
Toksaitov Dmitrii Alexandrovich <dmitrii@toksaitov.com>
2019-03-12 13:19:48 +0000

    [server] Add code to process and save new chirps to DB (closes #6)

--- chirper.js
+++ chirper.js
@@ -1,11 +1,33 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
+const mysql = require('mysql')
 
 require('dotenv').config()
 
 const port = process.env.CHIRPER_PORT
 
+const connection = mysql.createConnection({
+    host     : process.env.CHIRPER_DB_HOST,
+    user     : process.env.CHIRPER_DB_USER,
+    password : process.env.CHIRPER_DB_PASS,
+    database : process.env.CHIRPER_DB_NAME
+})
+
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ extended: true }))
 app.set('view engine', 'ejs')
+
+app.post('/', (request, response) => {
+    const content = request.body.content
+    connection.query(`INSERT INTO chirps SET ?;`, { 'content': content }, (error) => {
+        if (error) throw error
+
+        response.writeHead(302, {
+            'Location': '/'
+        });
+        response.end();
+    })
+})
 
 app.listen(port, () => console.log(`The Chirper server is listening on port ${port}.`))
